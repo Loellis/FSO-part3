19ba22a707a1a15d8172af0332f9f1bcccad2e39
Martin Kobro <loellis92@gmail.com>
2024-02-06 14:00:51 +0100

    Exercise 3.10 - Deployed backend via fly.io.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FSO-part3
 Following Full Stack Open course from University of Helsinki
+
+# URL
+The following url goes to the deployed backend:
+https://fso-part3.fly.dev/api/persons
--- index.js
+++ index.js
@@ -111,7 +111,7 @@ const generateId = (min, max) => {
   }
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server listening to port ${PORT}`)
 })
