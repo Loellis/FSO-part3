9b3dc302356560840c7ac58ad20ec92ae0e03ea2
Martin Kobro <loellis92@gmail.com>
2024-02-06 13:51:38 +0100

    Exercise 3.9 - Installed CORS to grant access to backend from the frontend.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token("json_data",  (request) => {
   if (request.method === "POST") {
